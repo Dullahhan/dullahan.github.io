380d88a20fa6eefc57fb9f7a7a8f9ce457573303
Dullahhan <130026493+Dullahhan@users.noreply.github.com>
2023-05-07 19:16:27 -0400

    initial commit

---
+++ database.js
@@ -0,0 +1,27 @@
+const sliderDb = [
+  {
+    title:"Titulo de ejemplo uno",
+    src:"img/img1.jpg"
+  },
+  {
+    title:"Titulo de ejemplo dos",
+    src:"img/img2.jpg"
+  },
+  {
+    title:"Titulo de ejemplo tres",
+    src:"img/img3.jpg"
+  },
+  {
+    title:"Titulo de ejemplo cuatro",
+    src:"img/img4.jpg"
+  },
+  {
+    title:"Titulo de ejemplo cinco",
+    src:"img/img5.jpg"
+  },
+  {
+    title:"Titulo de ejemplo seis",
+    src:"img/img6.jpg"
+  },
+
+]
